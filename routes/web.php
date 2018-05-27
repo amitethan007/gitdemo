15ac54e6e848bfdcda373aa5c3e43244d9cb83c4
Amit Kumar <amit@softwaysolutions.com>
2018-05-26 20:59:56 -0500

    feature2-fresh

--- routes/web.php
+++ routes/web.php
@@ -11,21 +11,13 @@
 |
 */
 
+
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('/forgotpassword', function () {
-    return view('forgotpassword');
-});
-
-Route::get('/forgotpassword-for-release-1-again', function () {
-    return view('forgotpassword');
-});
-
-
-Route::get('/release-2-feature', function () {
-    return view('forgotpassword');
+Route::get('/feature2', function () {
+    return view('feature2');
 });
 
